a3fe1b01dab5c6424568979b56131132935b311a
dartov <anton.zadorozhniy@gmail.com>
2021-07-19 11:49:44 +0200

    Added Feature Store demo

---
+++ model_definitions/87328c6e-f42f-4813-b3c0-64d25a1f9d83/model_modules/evaluation.py
@@ -0,0 +1,76 @@
+from sklearn import metrics
+from teradataml import create_context
+from teradataml.dataframe.dataframe import DataFrame
+from teradataml.dataframe.copy_to import copy_to_sql
+from aoa.stats import stats
+from aoa.util.artefacts import save_plot
+from tdextensions.featurestore import FeatureStore
+
+
+import os
+import joblib
+import json
+import numpy as np
+import pandas as pd
+
+
+def evaluate(data_conf, model_conf, **kwargs):
+    model = joblib.load('artifacts/input/model.joblib')
+
+    create_context(host=os.environ["AOA_CONN_HOST"],
+                   username=os.environ["AOA_CONN_USERNAME"],
+                   password=os.environ["AOA_CONN_PASSWORD"],
+                   database=data_conf["schema"] if "schema" in data_conf and data_conf["schema"] != "" else None)
+
+    fs = FeatureStore(get_connection(), data_conf['feature_store'])
+
+    entity = data_conf['entity']
+    test_dataset_instance_date = data_conf['instance_date']
+
+    # read test dataset from Feature Store and convert to Pandas Dataframe
+    test_df = fs.get_featureset_df(entity, test_dataset_instance_date, model.feature_names + [model.target_name])
+    test_pdf = test_df.to_pandas()
+
+    X_test = test_pdf[model.feature_names]
+    y_test = test_pdf[model.target_name]
+
+    print("Scoring")
+    y_pred = model.predict(test_pdf[model.feature_names])
+
+    y_pred_tdf = pd.DataFrame(y_pred, columns=[model.target_name])
+    y_pred_tdf["PatientId"] = test_pdf["PatientId"].values
+
+    evaluation = {
+        'Accuracy': '{:.2f}'.format(metrics.accuracy_score(y_test, y_pred)),
+        'Recall': '{:.2f}'.format(metrics.recall_score(y_test, y_pred)),
+        'Precision': '{:.2f}'.format(metrics.precision_score(y_test, y_pred)),
+        'f1-score': '{:.2f}'.format(metrics.f1_score(y_test, y_pred))
+    }
+
+    with open("artifacts/output/metrics.json", "w+") as f:
+        json.dump(evaluation, f)
+
+    metrics.plot_confusion_matrix(model, X_test, y_test)
+    save_plot('Confusion Matrix')
+
+    metrics.plot_roc_curve(model, X_test, y_test)
+    save_plot('ROC Curve')
+
+    # xgboost has its own feature importance plot support but lets use shap as explainability example
+    import shap
+
+    shap_explainer = shap.TreeExplainer(model['xgb'])
+    shap_values = shap_explainer.shap_values(X_test)
+
+    shap.summary_plot(shap_values, X_test, feature_names=model.feature_names,
+                      show=False, plot_size=(12, 8), plot_type='bar')
+    save_plot('SHAP Feature Importance')
+
+    feature_importance = pd.DataFrame(list(zip(model.feature_names, np.abs(shap_values).mean(0))),
+                                      columns=['col_name', 'feature_importance_vals'])
+    feature_importance = feature_importance.set_index("col_name").T.to_dict(orient='records')[0]
+
+    predictions_table="TMP_{}".format(data_conf["predictions"]).lower()
+    copy_to_sql(df=y_pred_tdf, table_name=predictions_table, index=False, if_exists="replace", temporary=True)
+
+    stats.record_evaluation_stats(test_df, DataFrame(predictions_table), feature_importance)
---
+++ model_definitions/87328c6e-f42f-4813-b3c0-64d25a1f9d83/model_modules/training.py
@@ -0,0 +1,72 @@
+from xgboost import XGBClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
+from nyoka import xgboost_to_pmml
+from teradataml import create_context
+from teradataml.dataframe.dataframe import DataFrame
+from tdextensions.featurestore import FeatureStore
+from aoa.stats import stats
+from aoa.util.artefacts import save_plot
+
+import joblib
+import os
+
+
+def train(data_conf, model_conf, **kwargs):
+    hyperparams = model_conf["hyperParameters"]
+
+    create_context(host=os.environ["AOA_CONN_HOST"],
+                   username=os.environ["AOA_CONN_USERNAME"],
+                   password=os.environ["AOA_CONN_PASSWORD"],
+                   database=data_conf["schema"] if "schema" in data_conf and data_conf["schema"] != "" else None)
+
+    fs = FeatureStore(get_connection(), data_conf['feature_store'])
+
+    entity = data_conf['entity']
+    feature_names = data_conf['feature_names'].split(',')
+    target_name = data_conf['target_name']
+    training_dataset_instance_date = data_conf['instance_date']
+
+    # read training dataset from Feature Store and convert to Pandas Dataframe
+    train_df = fs.get_featureset_df(entity, training_dataset_instance_date, feature_names + [target_name])
+    train_pdf = train_df.to_pandas()
+
+    # split data into X and y
+    X_train = train_pdf.drop(target_name, 1)
+    y_train = train_pdf[target_name]
+
+    print("Starting training...")
+
+    # fit model to training data
+    model = Pipeline([('scaler', MinMaxScaler()),
+                      ('xgb', XGBClassifier(eta=hyperparams["eta"],
+                                            max_depth=hyperparams["max_depth"]))])
+    # xgboost saves feature names but lets store on pipeline for easy access later
+    model.feature_names = feature_names
+    model.target_name = target_name
+
+    model.fit(X_train, y_train)
+
+    print("Finished training")
+
+    # export model artefacts
+    joblib.dump(model, "artifacts/output/model.joblib")
+
+    # we can also save as pmml so it can be used for In-Vantage scoring etc.
+    xgboost_to_pmml(pipeline=model, col_names=feature_names, target_name=target_name,
+                    pmml_f_name="artifacts/output/model.pmml")
+
+    print("Saved trained model")
+
+    from xgboost import plot_importance
+    model["xgb"].get_booster().feature_names = feature_names
+    plot_importance(model["xgb"].get_booster(), max_num_features=10)
+    save_plot("feature_importance.png")
+
+    feature_importance = model["xgb"].get_booster().get_score(importance_type="weight")
+    stats.record_training_stats(train_df,
+                       features=feature_names,
+                       predictors=[target_name],
+                       categorical=[target_name],
+                       importance=feature_importance,
+                       category_labels={target_name: {0: "false", 1: "true"}})
